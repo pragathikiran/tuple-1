98e4222cca720b524cf3cba7efd5ad5dc4854505
pragathikiran <pragathi20.kiran@gmail.com>
2025-04-04 20:42:18 +0530

    Add files via upload

---
+++ 15tuple.py
@@ -0,0 +1,34 @@
+my_tuple = ()
+print(my_tuple)
+
+my_tuple = (1,2,3)
+print(my_tuple)
+
+my_tuple = (1,"hi",3)
+print(my_tuple)
+
+my_tuple = ("bye",[6,7,8],(1,2,3))
+print(my_tuple)
+
+my_tuple = ('a','s','d','f','g','y','u','k',)
+print(my_tuple[4])
+print(my_tuple[3])
+
+n_tuple = ("bye",[6,7,8],(1,2,3))
+print(n_tuple[0][2])
+print(n_tuple[1][2])
+
+print("sliced:",my_tuple[1:4])
+
+for letter in (my_tuple):
+ print("hello",letter)
+
+
+
+
+
+
+
+
+
+
